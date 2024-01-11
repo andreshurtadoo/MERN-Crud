175a2a47b8d347d289d0404cbec7852d057f7d49
Andres Hurtado <hurtadoandres73@hotmail.com>
2024-01-11 03:15:04 -0400

    frontend setup ready with react and route

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom"
+import TasksPage from "./pages/TasksPage"
+import TaskForm from "./pages/TaskForm"
+import NotFound from "./pages/NotFound"
+
+export default function App() {
+  return (
+    <Routes >
+      <Route path="/" element={<TasksPage />} />
+      <Route path="/new" element={<TaskForm />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  )
+}
